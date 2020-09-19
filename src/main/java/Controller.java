33a2821467e2d06471272aa75d59c6fff708ee86
Lexxi <lskiner8577@eagle.fgcu.edu>
2020-09-19 13:48:44 -0400

    added a database and tables

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
@@ -9,4 +14,76 @@ public class Controller {
   public void recordProduction(MouseEvent mouseEvent) {
     System.out.println("clicked record production");
   }
+
+  public void initialize() {
+    connectToDB();
+  }
+
+  public void connectToDB() {
+    final String JDBC_DRIVER = "org.h2.Driver";
+
+    final String DB_URL = "jdbc:h2:./res/HR";
+
+
+    //  Database credentials
+
+    final String USER = "";
+
+    final String PASS = "";
+
+    Connection conn = null;
+
+    Statement stmt = null;
+
+
+
+    try {
+
+      // STEP 1: Register JDBC driver
+
+      Class.forName(JDBC_DRIVER);
+
+
+      //STEP 2: Open a connection
+
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+
+      //STEP 3: Execute a query
+
+      stmt = conn.createStatement();
+
+
+
+      String sql = "SELECT * FROM JOBS";
+
+
+      ResultSet rs = stmt.executeQuery(sql);
+
+      while (rs.next()) {
+
+        System.out.println(rs.getString(1));
+
+      }
+
+
+      // STEP 4: Clean-up environment
+
+      stmt.close();
+
+      conn.close();
+
+    } catch (ClassNotFoundException e) {
+
+      e.printStackTrace();
+
+
+    } catch (SQLException e) {
+
+      e.printStackTrace();
+
+    }
+
+  }
+
 }
