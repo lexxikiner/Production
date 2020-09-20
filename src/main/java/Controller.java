83ee5686ce3ad08d163b63678163b64d354a1682
Lexxi <lskiner8577@eagle.fgcu.edu>
2020-09-19 22:44:05 -0400

    added comments where they were needed

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,29 +1,76 @@
+/**
+ * The controller class for all three tabs, which include: Product Line, Produce, and Production
+ * Line
+ *
+ * @author Lexxi Kiner
+ * @date 9/19/2020
+ */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
+  @FXML
+  private Button addProduct;
+
+  @FXML
+  private TextField productName;
+
+  @FXML
+  private TextField manufacturer;
+
+  @FXML
+  private ChoiceBox<String> itemType;
+
+  /**
+   * @param mouseEvent - the program connects to the database once the button is pressed
+   * @return void
+   */
+  @FXML
   public void addProduct(MouseEvent mouseEvent) {
-    System.out.println("clicked add product");
+    connectToDB();
   }
 
+  /**
+   * @param mouseEvent - the program prints to the console once the recordProduction is clicked
+   * @return void
+   */
+  @FXML
   public void recordProduction(MouseEvent mouseEvent) {
     System.out.println("clicked record production");
   }
 
+  /**
+   * populates the choice box in the Production Line tab
+   *
+   * @param - none
+   * @return void
+   */
   public void initialize() {
-    connectToDB();
+    for (int count = 1; count <= 10; count++) {
+      itemType.getItems().add(String.valueOf(count));
+    }
   }
 
+  /**
+   * allows the user to interact with the database
+   *
+   * @param - none
+   * @return void
+   */
   public void connectToDB() {
     final String JDBC_DRIVER = "org.h2.Driver";
 
     final String DB_URL = "jdbc:h2:./res/HR";
-
 
     //  Database credentials
 
@@ -35,37 +82,42 @@ public class Controller {
 
     Statement stmt = null;
 
-
-
     try {
 
       // STEP 1: Register JDBC driver
 
       Class.forName(JDBC_DRIVER);
 
-
       //STEP 2: Open a connection
 
+      //identified as a bug, but i know that the password is empty for now
       conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
 
       //STEP 3: Execute a query
 
-      stmt = conn.createStatement();
+      stmt = conn.createStatement(); // identified as a bug
 
+      String prodName = productName.getText();
+      String manufacturerText = manufacturer.getText();
 
+      String sql = "INSERT INTO Product(type, manufacturer, name)"
+          + "VALUES ( 'AUDIO', 'Apple', 'iPod' )";
 
-      String sql = "SELECT * FROM JOBS";
+      stmt.executeUpdate(sql);
 
+      String sql2 = "SELECT name, manufacturer, type "
+          + "FROM product ";
 
-      ResultSet rs = stmt.executeQuery(sql);
+      ResultSet rs = stmt.executeQuery(sql2); //identified as a bug
 
+      // print out the 3 columns of the table
       while (rs.next()) {
 
-        System.out.println(rs.getString(1));
+        System.out.print(rs.getString(1) + " ");
+        System.out.print(rs.getString(2) + " ");
+        System.out.println(rs.getString(3));
 
       }
-
 
       // STEP 4: Clean-up environment
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+/**
+ * The main class for the Production Project
+ *
+ * @author Lexxi Kiner
+ * @date 9/19/2020
+ */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,16 +13,29 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
   @Override
+  /**
+   * this is where the sample.fxml and style.css are used to create the visual of the tabs and
+   * tab pane window
+   * @param primaryStage - creates the window
+   * @return void
+   */
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("TabPane Example");
+    primaryStage.setTitle("Lexxi Kiner - Production Project");
     Scene scene = new Scene(root);
     scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
     primaryStage.setScene(scene);
     primaryStage.show();
   }
 
+  /**
+   * the main class of the Production Project
+   *
+   * @param args
+   * @return void
+   */
   public static void main(String[] args) {
     launch(args);
   }
